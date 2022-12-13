05eefbbf43975cd69353935be5ac6a98dcd04e49
HumbertoJr10 <humbertojrguerra@gmail.com>
2022-12-12 21:00:09 -0400

    Mostramos peso y tamaño y temperamentos en las cards

--- client/src/components/Dogs/Dogs.jsx
+++ client/src/components/Dogs/Dogs.jsx
@@ -2,19 +2,34 @@ import React from "react"
 import styles from './Dogs.module.css'
 
 
-export default function Dogs ( { name, heightMin, heightMax, weightMin, weightMax, life_span, image, temperament } ) {
+export default function Dogs ( { 
+    name, 
+    heightMin, 
+    heightMax, 
+    weightMin, 
+    weightMax, 
+    life_span, 
+    image, 
+    temperament 
+}) {
     return (
         <div className={styles.container}>
             <div className={styles.imgSide}>
                 <img className={styles.imagen} src={image|| "http://pawrider.com/assets/images/pages-loder.gif"} alt='none' />
+                <div className={styles.statsContainer}>
+                </div>
+                <div className={styles.stats}>
+                    <p>Height: {heightMin} - {heightMax} Cm</p>
+                    <p>Weight: {weightMin} - {weightMax} Kg</p>
+                </div>
+
             </div>
             <div className={styles.NameSide}>
-                <h2 className={styles.CardName}> {name || "undefined"} </h2>
+                <h2 className={styles.CardName}> {name || "404 ERROR"} </h2>
             </div>
             
             <div className={styles.FooterSide}>
-                <p>Height: {heightMin} - {heightMax} Cm</p>//
-                <p>Weight: {weightMin} - {weightMax}</p>
+                <p>{temperament || 'Unknown'}</p>
             </div>
         </div>
     )
