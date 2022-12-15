4375d98c879689801e7c84b13f04452ab172a703
HumbertoJr10 <humbertojrguerra@gmail.com>
2022-12-14 23:34:37 -0400

    fix bug

--- client/src/components/Dogs/Dogs.jsx
+++ client/src/components/Dogs/Dogs.jsx
@@ -18,26 +18,26 @@ export default function Dogs ( {
     const Dark = useSelector(state => state.DarkMode)
 
     return (
-        <NavLink to={`/Breed/${name}`}>
-        <div className={Dark?styles.container_dark:styles.container}>
-            <div className={styles.imgSide}>
-                <img className={styles.imagen} src={image|| "http://pawrider.com/assets/images/pages-loder.gif"} alt='none' />
-                <div className={styles.statsContainer}>
-                </div>
-                <div className={styles.stats}>
-                    <p>Height: {heightMin} - {heightMax} Cm</p>
-                    <p>Weight: {weightMin} - {weightMax} Kg</p>
-                </div>
+        <NavLink to={`/Breed/${name}`} className={styles.link}>
+            <div className={Dark?styles.container_dark:styles.container}>
+                <div className={styles.imgSide}>
+                    <img className={styles.imagen} src={image|| "http://pawrider.com/assets/images/pages-loder.gif"} alt='none' />
+                    <div className={styles.statsContainer}>
+                    </div>
+                    <div className={styles.stats}>
+                        <p>Height: {heightMin} - {heightMax} Cm</p>
+                        <p>Weight: {weightMin} - {weightMax} Kg</p>
+                    </div>
 
+                </div>
+                <div className={Dark?styles.NameSide_dark:styles.NameSide}>
+                    <h2 className={Dark?styles.CardName_dark:styles.CardName}> {name || "404 ERROR"} </h2>
+                </div>
+                
+                <div className={Dark?styles.FooterSide_dark:styles.FooterSide}>
+                    <p className={styles.temper}>{temperament || 'Unknown'}</p>
+                </div>
             </div>
-            <div className={Dark?styles.NameSide_dark:styles.NameSide}>
-                <h2 className={Dark?styles.CardName_dark:styles.CardName}> {name || "404 ERROR"} </h2>
-            </div>
-            
-            <div className={Dark?styles.FooterSide_dark:styles.FooterSide}>
-                <p>{temperament || 'Unknown'}</p>
-            </div>
-        </div>
         </NavLink>
     )
 }
