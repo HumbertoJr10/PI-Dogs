cc6fce49f0e700006c1aad06f6fe2da7db82cab8
HumbertoJr10 <humbertojrguerra@gmail.com>
2022-12-19 18:30:03 -0400

    Modelo User y ruta empezada

--- api/src/db.js
+++ api/src/db.js
@@ -27,7 +27,9 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog, Temperament } = sequelize.models;
+const { Dog, Temperament, User } = sequelize.models;
+
+console.log(sequelize.models)
 
 // RELACIONES
 Dog.belongsToMany(Temperament, {through: 'dogs_temperament'});
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const dogsRouter = require('./dogRoute');
 const temperamentsRouter = require("./temperamentRoute")
+const userRouter = require("./userRoute")
 
 const router = Router();
 
 // Rutas Modularizadas
 
+router.use('/user', userRouter)
 router.use('/dogs', dogsRouter)
 router.use('/temperaments', temperamentsRouter)
 
