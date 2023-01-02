abdd62e1e915d262c6da43b5422f420189a0a274
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-01-02 12:32:50 -0400

    alter true

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const { PORT } = require('./config')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`------Server Escuchando en el puerto ${PORT}------`); // eslint-disable-line no-console
   });
